32ae3ec6e2c8b81e4dc6533cf2749fd0a2996656
Mushahid Khan <mushahid.khan@codeb.online>
2020-08-27 01:56:16 +0530

    login and registration form added with spin loader.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: any;
+  password: any;
+  emailid: any;
+  name: any;
+  registeredUser = []
+  isLogin: boolean = false;
+  errorMessage: any;
+  isSignUp: boolean = false;
+  constructor(public router: Router, public spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem("isLogin")) {
+      this.router.navigate([`/dashboard`])
+    }
+  }
+
+  login() {
+    console.log("UserName", this.username, "Passwrod", this.password);
+    if (this.username == 'admin@admin.com' && this.password == 'pyxis@321') {
+      localStorage.setItem("isLogin", 'true');
+      this.errorMessage = "";
+      this.router.navigate([`/dashboard`]);
+    } else {
+      this.errorMessage = "Please check your username and password is wrong."
+    }
+  }
+  createNewAccount() {
+    this.isSignUp = true;
+  }
+  signUp() {
+
+    this.spinner.show()
+    let registration = {
+      name: this.name,
+      username: this.emailid,
+      password: this.password
+    }
+
+    this.registeredUser.push(registration);
+
+    console.log("Total Registered User", this.registeredUser);
+    setTimeout(() => {
+      this.isSignUp = false;
+
+      this.spinner.hide()
+    }, 1000);
+
   }
 
 }
