8e820a48132683f4cb9b9f5dd98c04f67e443428
Mushahid Khan <mushahid.khan@codeb.online>
2020-08-28 02:56:02 +0530

    displaying the question list on dashboard page.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,16 +8,57 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
+
+  orderSuffalList = [];
+
+  questionList = [
+    {
+      ques: "Who is the prime minister of India?",
+      ans: "Narender Modi"
+    },
+    {
+      ques: "Who is the president of India?",
+      ans: "Ram Nath Kobind"
+    },
+    {
+      ques: "Who is the home minister of India?",
+      ans: "Amit Shah"
+    },
+    {
+      ques: "Who is the chif minister of Uttar Pradesh ?",
+      ans: "Yogi Aditiya Nath"
+    },
+    {
+      ques: "Who is the chif minister of Rajisthan?",
+      ans: "Ashok Gehlot"
+    }
+  ]
+
+
   constructor(public router: Router) { }
 
 
   ngOnInit(): void {
-    if (!(localStorage.getItem('isLogin')) && !localStorage.getItem('username')) {
 
+    //Suffaling the question in array.
+    if (JSON.parse(localStorage.getItem("orderSuffalList"))) {
+      this.questionList = JSON.parse(localStorage.getItem("orderSuffalList"));
+    }
+    for (let i = 0; i < this.questionList.length; i++) {
+      this.orderSuffalList.push(this.questionList[i]);
+
+    }
+    this.orderSuffalList.push(this.questionList[0]);
+    this.orderSuffalList.splice(0, 1);
+    localStorage.setItem("orderSuffalList", JSON.stringify(this.orderSuffalList))
+
+    //checking the user is loged in or not.
+    if (!(localStorage.getItem('isLogin')) && !localStorage.getItem('username')) {
       this.router.navigate([`/`])
 
     }
   }
+
 
   logOut() {
     localStorage.removeItem('isLogin');
